d68dcc314b6d5735c5564bf0c8a48491f9aadfc1
Tanmay7100Jain <tanmayjain.24.dce@iite.indusuni.ac.in>
2025-06-12 03:33:53 +0530

    Complete

--- src/Components/Wronganswer.tsx
+++ src/Components/Wronganswer.tsx
@@ -1,28 +1,24 @@
 import React from "react";
 import { Ask } from "./Ask";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+
 const Wronganswer = () => {
-  useEffect(() => {
-    <Wronganswer />;
-  }, []);
   const navigate = useNavigate();
 
 
   return (
-    <>
-      <div className="text-white text-center text-4xl">
-        <span className=" flex justify-center item-center">
-          {Ask()[Math.floor(Math.random() * Ask().length)]}
-        </span>
+    <div className="h-screen flex flex-col justify-center items-center  text-white text-center">
+      <div className="text-4xl mb-28">
+        {Ask()[Math.floor(Math.random() * Ask().length)]}
+      </div>
       <button
         onClick={() => navigate("/Starting")}
-        className="px-6 py-2 mt-9 bg-white text-black rounded hover:bg-gray-200 text-lg"
-        >
+        className="px-6 py-2 bg-white text-black rounded hover:bg-gray-200 text-lg"
+      >
         Play Again
       </button>
-        </div>
-    </>
+    </div>
   );
 };
+
 export default Wronganswer;
